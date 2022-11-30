5396c1acdde270564adb057f9a379523a114f93a
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-11-30 16:54:26 +0700

    rename program and add function for menu program

--- Stack_A/Program.cs
+++ Stack_A/Program.cs
@@ -61,11 +61,46 @@ namespace Stack_A
                 Console.WriteLine();
             }
         }
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n**************Stack Menu*************\n");
+                Console.WriteLine("1.Push");
+                Console.WriteLine("2.Pop");
+                Console.WriteLine("3.Display");
+                Console.WriteLine("4.Exit");
+                Console.WriteLine("\nenter your choice");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a Number : ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.Push(num);
+                        break;
+                    case '2':
+                        if (s.Empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+            }
         }
     }
+  
 }
